154877c1e51baa5a327c78e4cbd64f55ee2d5e48
Hamirwasia <ssacha@a483e7302e4c.ant.amazon.com>
2021-05-04 17:29:45 -0500

    Updated output variable to show base url

--- output.tf
+++ output.tf
@@ -3,9 +3,9 @@ output "alb_arn" {
   value                   = aws_lb.alb.arn 
 }
 
-output "alb_dns" {
+output "alb_url" {
   description             = "DNS name of ALB fronting ECS cluster"
-  value                   = aws_lb.alb.dns_name 
+  value                   = "http://${aws_lb.alb.dns_name}:${var.alb_listen_port}"
 }
 
 /*
